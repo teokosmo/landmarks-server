3a47bd7208b971fcfa886b3a49ea7e35f5347b44
t.kosmopoulos <t.kosmopoulos@athexgroup.gr>
2021-04-04 12:55:04 +0300

    feat: set file maxUploadSize

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
+// https://parseplatform.org/parse-server/api/4.5.0/ParseServerOptions.html
 const parseServerConfig = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
@@ -22,6 +23,7 @@ const parseServerConfig = {
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
+  maxUploadSize: '5mb'
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
